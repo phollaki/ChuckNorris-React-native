d194d327d4e377fad7976463cbc3855001d4bf83
phollaki <phollakos15@gmail.com>
2020-12-19 22:03:07 +0100

    add favourite button
    favourite and unfavourite buttons added

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaView, Text, StyleSheet} from 'react-native';
-import Chucknorris from './src/Chucknorris';
+import Chucknorris from './src/ChuckNorris';
+
+
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container} >
       <StatusBar style="auto" />
-      <Chucknorris title="Chuck Norris Jokes"/>
+      <Chucknorris></Chucknorris>
     </SafeAreaView>
   );
 }
--- src/ChuckNorris.js
+++ src/ChuckNorris.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react'
-import {View, Text, ActivityIndicator, Image, Button} from 'react-native'
+import {View, Text, ActivityIndicator, Image, Button, FlatList} from 'react-native'
+
+
 
 const Chucknorris = ({title}) => {
     const [jokes, setJokes] = useState([])
-    const [index, setIndex] = useState(1)
+    const [index, setIndex] = useState(0)
+    const [favourites, setFavourites] = useState([])
 
     const load = async () => {
         const response = await fetch(
@@ -29,22 +32,54 @@ const Chucknorris = ({title}) => {
         setIndex(Math.floor(Math.random()* Math.floor(573)))
     }
 
+    const favourite = (id) => {
+        if(!favourites.includes(id)){
+            setFavourites([...favourites, id])
+        }
+    }
+    const unfavourite = (id) => {
+        const filtered = favourites.filter((favourite) => favourite !== id)
+        setFavourites(filtered)
+    }
+
+    const renderCell = () => {
+
+    }
+
     return (
-    <View style={{ flex: 1, alignItems: 'center' }}>
+    <View style={{flex: 1, alignItems: 'center' }}>
 
         <Text style={{ color: 'yellow', fontWeight:'bold',fontSize: 40, marginBottom: 10}}>{title}</Text>
 
         <Image 
         source={{ uri: 'http://pngimg.com/uploads/chuck_norris/chuck_norris_PNG15.png',}} 
-        style={{ width: 400, height: 400}}/>
+        style={{ width: 300, height: 300}}
+        resizeMode = 'contain'/>
 
-        <Text style={{ width: 400, textAlign:'center',color:'yellow'}}>{jokes.value[index].joke}</Text>
+        <Text style={{ width: 300, textAlign:'center',color:'yellow'}}>{jokes.value[index].joke}</Text>
 
-        <Button 
-        title="Give Me a Joke!"
-        color='red'
-        onPress={generate}/>
+        <Text style={{ color:'yellow'}}>{JSON.stringify(favourites)}</Text>
+        
+        
+        <View style={{flexDirection:'row'}}>
+        { favourites.includes (jokes.value[index].id) ? (
+            <
+            Button title ="Unfavourites" onPress={() => unfavourite (jokes.value[index].id)} 
+            />
+        ) : (
+            <
+            Button title ="Favourites" onPress={() => favourite (jokes.value[index].id)} 
+            />
+        )
+        }
+        <Button title="Give me a Joke!" color="red" onPress={generate}/>
+        </View>
 
+        <FlatList
+        data = {favourites}
+        renderItem = {renderCell}
+        />
+        
     </View>
     )
 }
