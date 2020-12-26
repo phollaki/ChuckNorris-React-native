c026d3abfd57febdf2ee06086cc0a040972891bd
phollaki <phollakos15@gmail.com>
2020-12-26 16:37:33 +0100

    Delete from favourites

--- src/Favourites.js
+++ src/Favourites.js
@@ -1,16 +1,21 @@
 import React from 'react'
-import { render } from 'react-dom'
-import { SafeAreaView } from 'react-native'
 import {FlatList, Text, TouchableHighlight} from 'react-native'
 import Swipeout from 'react-native-swipeout'
 import { useFavourites } from './hooks/useFavourites'
 import { useJokes } from './hooks/useJokes'
+import { Feather } from '@expo/vector-icons'; 
+
 
 const Favourites = ({ navigation }) => {
     const {favourites, add, remove} = useFavourites()
-    const {jokes, setIndex} = useJokes()
+    const {jokes, index, setIndex} = useJokes()
 
     const renderCell = ({item}) => (
+        <Swipeout
+        right = {swipeoutBtns}
+        autoClose
+        backgroundColor = 'transparent'
+        >
         <TouchableHighlight 
         key={item}
         underlayColor="grey"
@@ -25,7 +30,8 @@ const Favourites = ({ navigation }) => {
             }}
             >
             <Text style={{color:'yellow'}}>{getJokeForID(item)}</Text>
-            </TouchableHighlight>
+        </TouchableHighlight>
+        </Swipeout>
     )
     
     const getJokeForID = id => {
@@ -37,15 +43,27 @@ const Favourites = ({ navigation }) => {
         const index = jokes.value.findIndex((joke)=> joke.id === id)
         setIndex(index)
     }
+
+    const deleteJoke = (id) => {
+        remove(id)
+    }
     
-    
+    var swipeoutBtns = [
+        {
+          text: <Feather name="trash-2" size={28} color="black" />,
+          backgroundColor: '#ff0000',
+          onPress: () => { remove(jokes.value[index].id) }
+          
+        }
+      ]
+
     return (
             <FlatList 
             style={{width: '100%', backgroundColor: 'black'}}
             data={favourites}
             renderItem={renderCell}
             keyExtractor={(item)=> item.toString()}
-            />
+            />  
     )
 }
 export default Favourites
